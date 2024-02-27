70add4e554d4a77b5a6891259889caab35bcce13
edgarlevyings44 <edgarlevy81@gmail.com>
2024-02-28 00:59:47 +0300

    Perform CRUD

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,44 @@
 import express, { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client'
 
 const app = express()
 const port: number = 3000;
+
+const prisma = new PrismaClient()
+
+async function main() {
+  // ... you will write your Prisma Client queries here
+  // await prisma.user.create({
+  //   data: {
+  //     name: 'Alice',
+  //     email: 'alice@example.com',
+  //     posts: {
+  //       create: { title: 'Hello World' },
+  //     },
+  //     profile: {
+  //       create: { bio: 'I like turtles' },
+  //     },
+  //   },
+  // })
+
+  // const allUsers = await prisma.user.findMany({
+  //   include: {
+  //     posts: true,
+  //     profile: true,
+  //   },
+  // })
+  // console.dir(allUsers, { depth: null })
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Server Running!');
